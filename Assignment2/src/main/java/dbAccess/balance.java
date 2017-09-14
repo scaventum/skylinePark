8ae21864318cf3fc3395f502bbdaf6d5c25a13b7
sar <sar@pawan.PAWAN>
2017-09-14 13:44:58 +1200

    logic of balance

---
+++ Assignment2/src/main/java/dbAccess/balance.java
@@ -0,0 +1,67 @@
+package dbAccess;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class balance
+ */
+public class balance extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       private DBConnection newConnection;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public balance() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	String Action = request.getParameter("Action");
+	
+	if(Action.equals("getBalance")) {
+		try {
+			newConnection = new DBConnection();
+			String CarPlateNO = request.getParameter("CarPlateNo");
+			String Balance = newConnection.getValue("Select Sum(Amout) From balance Where CarPlateNO = '"+CarPlateNO+"'");
+			if (Balance.equals("")) {
+				Balance="";
+			}
+			else {
+				Balance="$"+Balance;
+			}
+			
+			response.setContentType("text/plain");
+			response.getWriter().write(Balance);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}catch(Exception e) {
+			e.printStackTrace();
+		
+			
+			}
+		}
+	}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- Assignment2/src/main/webapp/views/balance.jsp
+++ Assignment2/src/main/webapp/views/balance.jsp
@@ -1,4 +1,24 @@
-<div id="content">
+ 
+ <script>
+$(document).ready(function() {
+	
+
+	$("input[name='txtCarPlateNO']").change(function(e){
+		var CarPlateNO = $(this).val();
+		$.get('balance', {
+			Action : "getBalance",
+			CarPlateNO : CarPlateNO
+		}, function(result) {
+			$("input[name='Balance']").val(result);
+		});
+	});
+ 
+});
+</script>
+ 
+ 
+ 
+ <div id="content">
 	<div class="label100"> Carplate number </div>
 	<div class="vMargin" style="height:5px;"></div>
 	<input class="input100" name="txtCarPlateNO" maxlength=6  minlength=6 required>
@@ -7,5 +27,6 @@
 	<div class="vMargin" style="height:5px;"></div>
 	<input class="input100" name="Balance" maxlength=6  minlength=6 required>
 		<div class="vMargin" style="height:10px;"></div>
-		<input type="button" name="close" value="Close " class="btnForm btnMinute"/>
+		<a href="index.jsp"><input type="button" name="close" value="Close " class="btnForm btnMinute"/>
+		</a>
 </div>
